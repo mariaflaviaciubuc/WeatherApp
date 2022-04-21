f988b1df537549492c9e572e038606740fe06503
mariaflaviaciubuc <ciubucmaria17@stud.ase.ro>
2022-04-22 02:10:02 +0300

    remove API key

--- Weather/WeatherApp/Constants.cs
+++ Weather/WeatherApp/Constants.cs
@@ -3,6 +3,6 @@
     public static class Constants
     {
         public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-        public static string OpenWeatherMapAPIKey = "7ce47e750cdc0dcabf5e3a9ea67b3221";
+        public static string OpenWeatherMapAPIKey = "";
     }
 }

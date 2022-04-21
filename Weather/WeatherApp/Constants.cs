52ae3de9dfe2b4a2182ce5c3167c13cb3bb7848c
mariaflaviaciubuc <ciubucmaria17@stud.ase.ro>
2022-04-22 00:33:45 +0300

    Add project files.

---
+++ Weather/WeatherApp/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "7ce47e750cdc0dcabf5e3a9ea67b3221";
+    }
+}
